2229a3a0e7639dc05ebd55b05b08cda122426831
Nicolas Giordano <nickgior@gmail.com>
2021-06-21 19:23:44 -0700

    added git and resume links

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const backToTop = document.getElementById('back-to-top');
-
+const git = document.getElementById('git');
+const resume = document.getElementById('resume');
+const about = document.getElementById('about')
+console.log(git, resume);
 window.addEventListener("scroll", showButton)
+window.addEventListener("scroll", changeColor)
+
+function changeColor() {
+    if (window.pageYOffset > about.offsetTop - 10) {
+        git.style.color = '#212127'
+        resume.style.color = '#212127'
+    } else {
+        git.style.color = '#fbfbfd'
+        resume.style.color = '#fbfbfd'
+    }
+}
 
 function showButton() {
     if (window.pageYOffset > 300) {
